b5ce9db1fd5df6e078c9690c0d5df6391870e29c
Victor Da Luz <vdaluz@gmail.com>
2014-11-20 23:20:26 -0600

    Install Swagger

--- demo/app/controllers/api/v1/base.rb
+++ demo/app/controllers/api/v1/base.rb
@@ -5,6 +5,10 @@ module API
       mount API::V1::Authors
       mount API::V1::Articles
 
+      add_swagger_documentation base_path: "/api",
+                                api_version: 'v1',
+                                hide_documentation_path: true
+
     end
   end
 end
